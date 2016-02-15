4f3f14a54b30f93a655e5d3cc0a782f687951550
Tom McLean <tom.a.mclean@gmail.com>
2016-02-15 12:01:33 +1100

    Make import work

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,19 +2,50 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 var express = require('express');
 
-var sources = {
-    scripts: 'lib/**/*.js',
+var watchify = require('watchify');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var globby = require('globby');
+var through = require('through2');
+
+var sources = { 
+    scripts: 'lib/**/*.js', 
     templates: 'templates/**/*.html'
 };  
 
+function compile(watch) {
+    var bundler = browserify('./lib/main.js', { debug: true })
+                .transform(babelify, { presets: ['es2015'] } );
+
+    function rebundle() {
+        return bundler.bundle()
+            .on('error', function(err) { console.error(err); this.emit('end'); })
+            .pipe(source('main.js'))
+            .pipe(buffer())
+            .pipe($.sourcemaps.init({ loadMaps: true }))
+            .pipe($.sourcemaps.write('./'))
+            .pipe(gulp.dest('./dist'));
+    }
+
+    if (watch) {
+        var watcher = watchify(bundler);
+        return watcher.on('update', function() {
+            console.log('-> bundling...');
+            rebundle();
+        });
+    } else {
+        return rebundle();
+    }
+}
+
+function watch() {
+    return compile(true);
+};
+
 gulp.task('scripts', function() {
-   return gulp.src(sources.scripts)
-        .pipe($.plumber())
-        .pipe($.sourcemaps.init())
-        .pipe($.babel({ presets: ['es2015'] }))
-        .pipe($.concat('main.js'))
-        .pipe($.sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'));
+    return compile();
 });
 
 gulp.task('templates', function() {
@@ -26,8 +57,8 @@ gulp.task('templates', function() {
 });
 
 gulp.task('watch', function(done) {
-    gulp.watch(sources.scripts, ['scripts']);
     gulp.watch(sources.templates, ['templates']);
+    compile(true);
 });
 
 gulp.task('build', ['scripts', 'templates'], function() {
